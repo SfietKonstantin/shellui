9aeaac5c122b26dda090c6b1ac7ff1b9dd54c5cd
Lucien XU <sfietkonstantin@free.fr>
2024-07-03 18:49:02 +0200

    Support extra operation around prompt when querying

--- shellui/src/input.rs
+++ shellui/src/input.rs
@@ -4,6 +4,9 @@ use std::io::{Error, ErrorKind, Result};
 pub trait OrElseQuery {
     type Output;
     fn or_else_query(self, label: &str) -> Result<Self::Output>;
+    fn or_else_query_with<F>(self, request: F) -> Result<Self::Output>
+    where
+        F: FnOnce(fn(&str) -> Result<Self::Output>) -> Result<Self::Output>;
 }
 
 impl<T> OrElseQuery for Option<T>
@@ -16,6 +19,16 @@ where
         match self {
             Some(value) => Ok(value.to_string()),
             None => get_string_input(label),
+        }
+    }
+
+    fn or_else_query_with<F>(self, request: F) -> Result<Self::Output>
+    where
+        F: FnOnce(fn(&str) -> Result<Self::Output>) -> Result<Self::Output>,
+    {
+        match self {
+            Some(value) => Ok(value.to_string()),
+            None => request(get_string_input),
         }
     }
 }
